f72c14eea682c4eb85080cbe69cee5542313b939
João Pedro Medeiros de Farias <jpmedeiros_farias@hotmail.com>
2018-06-23 18:25:43 -0300

    Script de criação das tabelas do coletor

--- Scripts/ColetorBD/criacao-tabela.sql
+++ Scripts/ColetorBD/criacao-tabela.sql
@@ -1,60 +1,44 @@
-ALTER SCHEMA `coletor`  DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci ;
-
-CREATE TABLE IF NOT EXISTS `coletor`.`Propriedade` (
-  `idPropriedade` INT(11) NOT NULL AUTO_INCREMENT,
-  `nomePropriedade` VARCHAR(45) NOT NULL,
-  `endereço` VARCHAR(255) NOT NULL,
-  `bairro` VARCHAR(45) NOT NULL,
-  `numero` INT(11) NOT NULL,
-  `complemento` VARCHAR(55) NULL DEFAULT NULL,
-  `proprietario` VARCHAR(55) NOT NULL,
-  `telefone` INT(11) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `numeroVacas` INT(11) NOT NULL,
-  `periodicidade` INT(11) NOT NULL,
-  `idColeta` INT(11) NOT NULL,
-  `idcidade` INT(11) NOT NULL,
-  PRIMARY KEY (`idPropriedade`),
-  INDEX `fk_Propriedade_Coleta_idx` (`idColeta` ASC),
-  INDEX `fk_Propriedade_cidade1_idx` (`idcidade` ASC),
-  CONSTRAINT `fk_Propriedade_Coleta`
-    FOREIGN KEY (`idColeta`)
-    REFERENCES `coletor`.`Coleta` (`idColeta`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Propriedade_cidade1`
-    FOREIGN KEY (`idcidade`)
-    REFERENCES `coletor`.`cidade` (`idcidade`)
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `coletor`.`Coleta` (
-  `idColeta` INT(11) NOT NULL AUTO_INCREMENT,
-  `qtde` INT(11) NOT NULL,
-  `data` DATE NOT NULL,
-  PRIMARY KEY (`idColeta`))
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `coletor`.`cidade` (
-  `idcidade` INT(11) NOT NULL AUTO_INCREMENT,
-  `nomeCidade` VARCHAR(45) NOT NULL,
-  `idEstado` INT(11) NOT NULL,
-  PRIMARY KEY (`idcidade`),
-  INDEX `fk_cidade_Estado1_idx` (`idEstado` ASC),
-  CONSTRAINT `fk_cidade_Estado1`
-    FOREIGN KEY (`idEstado`)
-    REFERENCES `coletor`.`Estado` (`idEstado`)
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `coletor`.`Estado` (
-  `idEstado` INT(11) NOT NULL AUTO_INCREMENT,
-  `Nome` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`idEstado`))
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `coletor`.`Usuario` (
-  `idUsuario` INT(11) NOT NULL,
-  `nomeUser` VARCHAR(45) NOT NULL,
-  `loginUser` VARCHAR(45) NOT NULL,
-  `senhaUser` VARCHAR(15) NOT NULL,
-  PRIMARY KEY (`idUsuario`))
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE IF NOT EXISTS Estados (
+  id        int               NOT NULL AUTO_INCREMENT,
+  nome      varchar(90)       NOT NULL,
+  sigla     varchar(2)        NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS Cidades (        
+  id        int               NOT NULL AUTO_INCREMENT,
+  nome      varchar(90)       NOT NULL,
+  id_estado int               NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_Cidades_Estados FOREIGN KEY (id_estado) REFERENCES Estados(id)
+);
+CREATE TABLE IF NOT EXISTS Propriedades (
+  id              int         NOT NULL AUTO_INCREMENT,
+  nome            varchar(90) NOT NULL,
+  cnpj            varchar(90) NOT NULL,
+  id_cidade       int         NOT NULL,
+  endereco        varchar(90) NOT NULL,
+  bairro          varchar(90) NOT NULL,
+  numero          varchar(20) NOT NULL,
+  complemento     varchar(90)     NULL,
+  proprietario    varchar(90) NOT NULL,
+  telefone        varchar(15) NOT NULL,
+  email           varchar(90) NOT NULL,
+  periodicidade   int         NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_Propriedades_Cidades FOREIGN KEY (id_cidade) REFERENCES Cidades(id)
+);
+CREATE TABLE IF NOT EXISTS Coletas (
+  id              int         NOT NULL AUTO_INCREMENT,
+  id_propriedade  int         NOT NULL,
+  quantidade      decimal     NOT NULL,
+  data_hora       datetime    NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_Coletas_Proprietario FOREIGN KEY (id_propriedade) REFERENCES Propriedades(id)
+);
+CREATE TABLE IF NOT EXISTS Usuarios (
+  id        int               NOT NULL AUTO_INCREMENT,
+  nome      varchar(90)       NOT NULL,
+  login     varchar(90)       NOT NULL,
+  senha     varchar(90)       NOT NULL,
+  PRIMARY KEY (id)
+);
