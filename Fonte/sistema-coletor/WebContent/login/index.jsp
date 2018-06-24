c2d794718aedd7a6abfb5a6eb402f5d741494a23
João Pedro Medeiros de Farias <jpmedeiros_farias@hotmail.com>
2018-06-24 11:14:12 -0300

    Implementação da tela de login

---
+++ Fonte/sistema-coletor/WebContent/login/index.jsp
@@ -0,0 +1,49 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="styles/styleLogin.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="login-page">
+        <div class="login-box">
+            <div class="side-left">
+                <div class="conteudo">
+                    <h2>Milk Colector</h2>
+                    <p> Deixandos seu dia mais fresco desde 1920!</p>
+                </div>
+            </div>
+            <div class="side-right">
+                <img src="images/caminhao.png" alt="Logo" class="img-circle"><spam class="welcome"> Seja Bem-Vindo(a)!</spam>
+                <h1>Login</h1>
+                <form action="" method="post">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
+                    <div class="pull-left">
+                        <br/>
+                        <br/>
+                        <input class="btn blue entrar" value="Entrar" type="submit">
+                    </div>
+<!--                     <div class="pull-right"> -->
+<!--                         <a href="/recuperar-senha" class="forget-pass">Esqueceu a senha?</a> -->
+<!--                         <br> -->
+<!--                     </div> -->
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- Fonte/sistema-coletor/src/com/dac/coletor/servlet/LoginServlet.java
+++ Fonte/sistema-coletor/src/com/dac/coletor/servlet/LoginServlet.java
@@ -18,65 +18,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet(name = "Login", urlPatterns = {"/Login"})
+@WebServlet(name = "Login", urlPatterns = { "/servlets/login" })
 public class LoginServlet extends HttpServlet {
-    
-  protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, NoSuchAlgorithmException {
-	  HttpSession session = request.getSession();
-	  session.setAttribute("usuario", null);
-	
-	  UsuarioDAO usuarioDAO = new UsuarioDAO();
-	  String login = request.getParameter("email");
-	  String senha = request.getParameter("password");
-	  try{
-		if(senha == null || senha.isEmpty() || login == null || login.isEmpty()){
-			RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
-			if(login != null && !login.isEmpty()) {
-			    request.setAttribute("email", login);
-			    request.setAttribute("msg", "Preencha o campo Password");
-			}
-	        rd.forward(request, response);
-	        return;
-		} else {
-    	    UsuarioBean usuario = usuarioDAO.validarLogin(login, senha);
-    	    if(usuario == null){
-    	      RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
-    	      request.setAttribute("msg", "Usuário/Senha inválidos");
-    	      request.setAttribute("email", login);
-    	      rd.forward(request, response);
-    	      return; 
-    	    } else {
-    	      session.setAttribute("usuario", usuario);
-    	      RequestDispatcher rd = getServletContext().getRequestDispatcher("/home.jsp");
-    	      rd.forward(request, response);
-    	      return;             
-    	    }
-		}
-	  } catch (SQLException ex) {
-		  ErrorHandler.handleException(request, ex);
-	  }
-  }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NoSuchAlgorithmException {
+        HttpSession session = request.getSession();
+        session.setAttribute("usuario", null);
+
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        String login = request.getParameter("email");
+        String senha = request.getParameter("password");
+
+        try {
+            if (senha == null || senha.isEmpty() || login == null || login.isEmpty()) {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/login");
+                if (login != null && !login.isEmpty()) {
+                    request.setAttribute("email", login);
+                    request.setAttribute("msg", "Preencha o campo Password");
+                }
+                rd.forward(request, response);
+
+            } else {
+                UsuarioBean usuario = usuarioDAO.validarLogin(login, senha);
+
+                if (usuario == null) {
+                    RequestDispatcher rd = getServletContext().getRequestDispatcher("/login");
+                    request.setAttribute("msg", "Usuário/Senha inválidos");
+                    request.setAttribute("email", login);
+                    rd.forward(request, response);
+
+                } else {
+                    session.setAttribute("usuario", usuario);
+                    RequestDispatcher rd = getServletContext().getRequestDispatcher("/home.jsp");
+                    rd.forward(request, response);
+                }
+            }
+        } catch (SQLException ex) {
+            ErrorHandler.handleException(request, ex);
+        }
+    }
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         try {
             processRequest(request, response);
         } catch (NoSuchAlgorithmException ex) {
             Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         try {
             processRequest(request, response);
         } catch (NoSuchAlgorithmException ex) {
