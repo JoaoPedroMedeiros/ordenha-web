ee5fdb78d163ca0e84443f896f44d94c3ddec5a4
Unknown <arion_assao@hotmail.com>
2018-06-24 17:23:55 -0300

    Script de cidades e estados brasileiros

---
+++ Scripts/ColetorBD/criacao-estados.sql
@@ -0,0 +1,38 @@
+-- Create Table --
+
+CREATE TABLE Estado (
+    id       INT          NOT NULL,
+    nome     VARCHAR (50) NOT NULL,
+    uf       CHAR 	 (2)  NOT NULL,
+    PRIMARY KEY (Id)
+);
+
+-- Insert Data --
+
+Insert into Estado (id, nome, uf) values (12, 'Acre', 'AC');
+Insert into Estado (id, nome, uf) values (27, 'Alagoas', 'AL');
+Insert into Estado (id, nome, uf) values (16, 'Amapá', 'AP');
+Insert into Estado (id, nome, uf) values (13, 'Amazonas', 'AM');
+Insert into Estado (id, nome, uf) values (29, 'Bahia', 'BA');
+Insert into Estado (id, nome, uf) values (23, 'Ceará', 'CE');
+Insert into Estado (id, nome, uf) values (53, 'Distrito Federal', 'DF');
+Insert into Estado (id, nome, uf) values (32, 'Espírito Santo', 'ES');
+Insert into Estado (id, nome, uf) values (52, 'Goiás', 'GO');
+Insert into Estado (id, nome, uf) values (21, 'Maranhão', 'MA');
+Insert into Estado (id, nome, uf) values (51, 'Mato Grosso', 'MT');
+Insert into Estado (id, nome, uf) values (50, 'Mato Grosso do Sul', 'MS');
+Insert into Estado (id, nome, uf) values (31, 'Minas Gerais', 'MG');
+Insert into Estado (id, nome, uf) values (15, 'Pará', 'PA');
+Insert into Estado (id, nome, uf) values (25, 'Paraíba', 'PB');
+Insert into Estado (id, nome, uf) values (41, 'Paraná', 'PR');
+Insert into Estado (id, nome, uf) values (26, 'Pernambuco', 'PE');
+Insert into Estado (id, nome, uf) values (22, 'Piauí', 'PI');
+Insert into Estado (id, nome, uf) values (33, 'Rio de Janeiro', 'RJ');
+Insert into Estado (id, nome, uf) values (24, 'Rio Grande do Norte', 'RN');
+Insert into Estado (id, nome, uf) values (43, 'Rio Grande do Sul', 'RS');
+Insert into Estado (id, nome, uf) values (11, 'Rondônia', 'RO');
+Insert into Estado (id, nome, uf) values (14, 'Roraima', 'RR');
+Insert into Estado (id, nome, uf) values (42, 'Santa Catarina', 'SC');
+Insert into Estado (id, nome, uf) values (35, 'São Paulo', 'SP');
+Insert into Estado (id, nome, uf) values (28, 'Sergipe', 'SE');
+Insert into Estado (id, nome, uf) values (17, 'Tocantins', 'TO');
